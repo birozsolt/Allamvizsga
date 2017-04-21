1d300ac2e6776012e6ab8c771763b8021e8d34b3
birozsolt <biro_zslt@yahoo.com>
2017-04-21 14:40:50 +0300

    openCV camera working

--- MyApplication/openCVLibrary2410/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ MyApplication/openCVLibrary2410/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\Egyetem\\Allamvizsga\\OpenCVProject\\openCVLibrary2410\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\Egyetem\\Allamvizsga\\Allamvizsga\\MyApplication\\openCVLibrary2410\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
