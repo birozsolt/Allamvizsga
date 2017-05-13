ad687dcb1de21d2fe1170b2b2dfa491d843a64cf
birozsolt <biro_zslt@yahoo.com>
2017-05-13 21:59:50 +0300

    Initial project with openCV nonfree module

---
+++ Project/app/src/main/java/com/biro/zsolt/android/cardrecognizer/MainActivity.java
@@ -0,0 +1,126 @@
+package com.biro.zsolt.android.cardrecognizer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Core;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("opencv_java");
+        System.loadLibrary("gnustl_shared");
+        System.loadLibrary("nonfree");
+        System.loadLibrary("sift-test");
+    }
+
+    Mat mRgba, mRgbaF, mRgbaT;
+    private CameraBridgeViewBase cameraBridgeViewBase;
+    private ImageView recognizedCard;
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    cameraBridgeViewBase.enableView();
+                    //System.loadLibrary("nonfree");
+                    //System.loadLibrary("sift-test");
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.show_camera);
+
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraView);
+        recognizedCard = (ImageView) findViewById(R.id.imageView);
+
+        runDemo();
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.textView);
+        //tv.setText(stringFromJNI());
+
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        recognizedCard.setBackgroundResource(R.drawable.ace_of_clubs);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_10, this, baseLoaderCallback);
+        } else {
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        Core.transpose(mRgba, mRgbaT);
+        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0, 0, 0);
+        Core.flip(mRgbaF, mRgba, 1);
+        return mRgba;
+    }
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native void runDemo();
+}
+
